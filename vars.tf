4eed5cf2f1e58f956e504052c2081e82f7865f85
naveen <naveenkumar9707@gmail.com>
2024-10-19 09:52:46 -0500

    docdb

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "subnets" {}
+variable "name" {
+  default = "docdb"
+}
+variable "tags" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_db_cidr" {}
+variable "engine_version" {}
+variable "kms_arn" {}
+variable "port_no" {
+  default = 27017
+}
